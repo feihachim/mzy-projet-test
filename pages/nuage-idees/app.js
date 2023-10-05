3440af8494f065d52444c1a2cc0c08e5df374ec0
Hachim Youssoufa <feihachim@yahoo.fr>
2023-10-05 16:10:36 +0200

    wrote some variables

--- pages/nuage-idees/app.js
+++ pages/nuage-idees/app.js
@@ -1,5 +1,11 @@
 "use strict";
 
 let chosenMovie=false;
+let genreList;
+let movieList;
+let movieListFiltered;
 const genreListBox=document.querySelector(".genre-list");
+const choiceButton=document.querySelector(".choice-button");
+
+// TODO nuage idee logique et display resultat
 
