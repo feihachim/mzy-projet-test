69d0c3886aadf360f3b00daeddfe984d0a822a7a
Hachim YOUSSOUFA <feihachim@yahoo.fr>
2023-10-08 12:53:11 +0200

    intégration de la page d'accueil et la landing page de Delphine

---
+++ pages/accueil/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const boxbox = document.querySelector(".box1");
+
+function changecouleur() {
+  boxbox.style.backgroundColor = "#1b4332";
+}
+
+function outcouleur() {
+  boxbox.style.backgroundColor = "#830B9D";
+}
